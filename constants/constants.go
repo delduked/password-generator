ea3b5172b5f3018ce50f998660e9632b0dcdc204
nate <nate.delduca@mintymint.info>
2021-12-19 14:46:42 -0500

    updating read me

--- constants/constants.go
+++ constants/constants.go
@@ -1,12 +1,6 @@
 package constants
 
 const (
-	//characters = []string{}
-	// Lower   = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
-	// Upper   = []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-	// Numbers = []string{`1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `0`}
-	// Special = []string{`!`, `"`, `#`, `$`, `%`, `&`, `'`, `(`, `)`, `*`, `+`, `,`, `-`, `.`, `/`, `:`, `;`, `<`, `=`, `>`, `?`, `@`, `[`, `\`, `]`, `^`, `_`, `{`, `|`, `}`, `~`}
-
 	Upper   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	Lower   = "abcdefghijklmnopqrstuvwxyz"
 	Numbers = "0123456789"
