e5f9d85e6d1aec1d40a0ae63f371bcc12a58ab66
nate <nate.delduca@mintymint.info>
2021-12-19 19:52:04 -0500

    add cors policy for everything

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"gitlab.com/alienate/password-generator/routes"
 )
 
 func main() {
 
 	app := fiber.New()
+
+	app.Use(cors.New())
 
 	app.Get("/healthcheck", routes.Health)
 	app.Post("/generateBody", routes.GenerateBody)
