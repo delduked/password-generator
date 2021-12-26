1640bd0cbd85d35c5cec8c0382da0dcdf46039e4
nate del duca <nate.delduca@gmail.com>
2021-12-26 21:59:41 +0000

    Update config/config.go

--- config/config.go
+++ config/config.go
@@ -9,6 +9,6 @@ import (
 var RedisCtx = context.Background()
 var Rdb = redis.NewClient(&redis.Options{
 	Addr:     "192.168.0.32:6379",
-	Password: "n4th4n43l", // no password set
+	Password: "", // no password set
 	DB:       0,           // use default DB
 })
