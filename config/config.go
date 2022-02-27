b50fd9d8d0bd05ae3004f4df03791d5838ff03cb
nate <nate@pop-os.localdomain>
2022-02-26 20:33:50 -0500

    removing jwt authentication

--- config/config.go
+++ config/config.go
@@ -9,6 +9,6 @@ import (
 var RedisCtx = context.Background()
 var Rdb = redis.NewClient(&redis.Options{
 	Addr:     "192.168.0.32:6379",
-	Password: "", // no password set
+	Password: "n4th4n43l", // no password set
 	DB:       0,           // use default DB
 })
--- image.dockerfile
+++ image.dockerfile
@@ -3,6 +3,5 @@ WORKDIR /api
 COPY . /api
 RUN go mod download
 RUN go get
-RUN go build
 EXPOSE 8080
-CMD [ "/api/main" ]
+CMD ["go","run","./main.go"]
